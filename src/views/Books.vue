b8bf6dc3dcc0743a9e9dce7bca6ed2ef454ccb9a
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-02 21:28:44 +0300

    Сделал начальный вид страницы с книгами

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -17,39 +17,50 @@ const books = useBooksStore();
       <span class="visually-hidenn">Предыдущий</span>
     </button>
 
-    <div>
-      <div v-for="i in Math.floor(books.books.length / 6)" v-bind:key="i">
-        <div class="books-library" v-for="j in 6" :key="j">
-          <div class="card book-card">
-            <h5 class="card-title text-break">{{ books.books[j + (i - 1) * 6 - 1].title }}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">
-              {{ books.books[j + (i - 1) * 6 - 1].author }}
-            </h6>
+    <div
+      id="caruselLibrary"
+      class="carousel carousel-dark slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div v-for="i in Math.floor(books.books.length / 12)" v-bind:key="i">
+          <div :class="{ active: i == 1 }" class="carousel-item">
+            <div class="books-library" v-for="j in 6" :key="j">
+              <div class="card book-card">
+                <h5 class="card-title text-break">
+                  {{ books.books[j + (i - 1) * 6 - 1].author }}
+                </h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">
+                  {{ books.books[j + (i - 1) * 6 - 1].title }}
+                </h6>
+              </div>
+            </div>
+            <br />
+            <div class="books-library" v-for="j in 6" :key="j">
+              <div class="card book-card">
+                <h5 class="card-title text-break">
+                  {{ books.books[j + (i - 1) * 6 - 1 + 6].author }}
+                </h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">
+                  {{ books.books[j + (i - 1) * 6 - 1].title }}
+                </h6>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
-      <div
-        id="caruselLibrary"
-        class="carousel carousel-dark slide"
-        data-bs-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">11111</div>
-          <div class="carousel-item">22222</div>
-          <div class="carousel-item">22222</div>
-        </div>
-      </div>
     </div>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#caruselLibrary"
-      data-bs-slide="next"
-    >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidenn">Следующий</span>
-    </button>
-    <div>ВПРАВО</div>
-  </div>
+  
+
+  <button
+    class="carousel-control-next"
+    type="button"
+    data-bs-target="#caruselLibrary"
+    data-bs-slide="next"
+  >
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidenn">Следующий</span>
+  </button>
+  <div>ВПРАВО</div>
+</div>
 </template>
