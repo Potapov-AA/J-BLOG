356e507acfebb3f172fbb5630eeb744d9a327659
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-02 19:50:39 +0300

    Добавил дефолтные значения для отладки

--- src/stores/books.js
+++ src/stores/books.js
@@ -2,10 +2,160 @@ import { defineStore } from "pinia";
 
 const STORE_NAME = "books";
 
+const defaultValue = [
+  {
+    id: 0,
+    title: "История с кладбищем",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 1,
+    title: "Никогде",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 2,
+    title: "Американские Боги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 3,
+    title: "Океан в конце дороги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 4,
+    title: "Звёздная пыль",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 5,
+    title: "Сыновья Ананси",
+    author: "Н. Гейман",
+    status: "Читаю",
+    isFavorite: false
+  },
+  {
+    id: 6,
+    title: "Благие знамения",
+    author: "Н. Гейман, Т. Пратчетт",
+    status: "Запланирована",
+    isFavorite: false
+  },
+  {
+    id: 0,
+    title: "История с кладбищем",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 1,
+    title: "Никогде",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 2,
+    title: "Американские Боги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 3,
+    title: "Океан в конце дороги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 4,
+    title: "Звёздная пыль",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 5,
+    title: "Сыновья Ананси",
+    author: "Н. Гейман",
+    status: "Читаю",
+    isFavorite: false
+  },
+  {
+    id: 6,
+    title: "Благие знамения",
+    author: "Н. Гейман, Т. Пратчетт",
+    status: "Запланирована",
+    isFavorite: false
+  },
+  {
+    id: 0,
+    title: "История с кладбищем",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 1,
+    title: "Никогде",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 2,
+    title: "Американские Боги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: false,
+  },
+  {
+    id: 3,
+    title: "Океан в конце дороги",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 4,
+    title: "Звёздная пыль",
+    author: "Н. Гейман",
+    status: "Прочтена",
+    isFavorite: true,
+  },
+  {
+    id: 5,
+    title: "Сыновья Ананси",
+    author: "Н. Гейман",
+    status: "Читаю",
+    isFavorite: false
+  },
+  {
+    id: 6,
+    title: "Благие знамения",
+    author: "Н. Гейман, Т. Пратчетт",
+    status: "Запланирована",
+    isFavorite: false
+  },
+];
+
 const getValue = () => {
   const value = localStorage.getItem(STORE_NAME);
 
-  return value ? JSON.parse(value) : [];
+  return value ? JSON.parse(value) : defaultValue;
 };
 
 export const useBooksStore = defineStore(STORE_NAME, {
@@ -13,17 +163,18 @@ export const useBooksStore = defineStore(STORE_NAME, {
     books: getValue(),
   }),
   actions: {
-    addBooks(title, author, status) {
+    addBooks(title, author, status, isFavorite) {
       let lengthArray = this.books.length;
 
       if (lengthArray == 0) {
-        this.books.push({ title, author, status, id: 0 });
+        this.books.push({id: 0, title, author, status, isFavorite});
       } else {
         this.books.push({
+          id: this.books[lengthArray-1].id++,
           title,
           author,
           status,
-          id: this.books[0].id++,
+          isFavorite
         });
       }
 
