c4bdd066dc13ecef7dd750b555cae404e08b9ac5
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-02 21:27:41 +0300

    Сделал компонент для статьей из devlog-а

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { useDevlogStore } from "@/stores/devlog";
+
+const props = defineProps({
+  id: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const devlog = useDevlogStore();
+
+function showText(id) {
+  const cardText = document.getElementById("article-" + id);
+
+  if (cardText.classList.value == "") {
+    cardText.classList.add("text-truncate");
+  } else {
+    cardText.classList.remove("text-truncate");
+  }
+}
+</script>
+
+<template>
+  <div
+    class="devlog-card border border-3 border-black mb-3"
+    @click="showText( devlog.articels[id].id)"
+  >
+    <div class="row">
+      <div class="col-12">
+        <b>&#60;dev#{{ devlog.articels[id].id + 1 }}&#62; </b>
+        <strong class="text-uppercase">
+          {{ devlog.articels[id].title }}
+        </strong>
+      </div>
+      <hr class="border border-dark border-1 opacity-100 mt-2" />
+    </div>
+
+    <p v-bind:id="'article-' + devlog.articels[id].id" class="text-truncate">
+      {{ devlog.articels[id].description }}
+    </p>
+  </div>
+</template>
+
+
