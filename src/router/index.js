b1df918b7f1f07dd6ff8d29f15b15a6908927521
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-02 17:34:54 +0300

    Добавил руты на новые страницы

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 import Books from '@/views/Books.vue'
+import Devlog from '@/views/Devlog.vue'
+import Recipes from '@/views/Recipes.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home},
-    { path: '/books', name: 'Books', component: Books}
+    { path: '/devlog', name: 'Devlog', component: Devlog},
+    { path: '/books', name: 'Books', component: Books},
+    { path: '/recipes', name: 'Recipes', component: Recipes}
 ]
 
 const router = createRouter({
