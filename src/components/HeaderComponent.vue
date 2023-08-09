176cffc4fdacb1630a0deeb0cdf8312e99b75115
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-09 13:08:11 +0300

    Компонент header-а

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderLinkComponent from "@/components/HeaderLinkComponent.vue";
+</script>
+
+<template>
+  <v-app-bar :elevation="8">
+    <v-app-bar-title>
+      <b>J-БЛОГ</b>
+    </v-app-bar-title>
+    <HeaderLinkComponent :icon="'mdi-home'" :link="'/'"></HeaderLinkComponent>
+    <HeaderLinkComponent :icon="'mdi-developer-board'" :link="'/devlog'"></HeaderLinkComponent>
+    <HeaderLinkComponent :icon="'mdi-bookshelf'" :link="'/books'"></HeaderLinkComponent>
+    <HeaderLinkComponent :icon="'mdi-chef-hat'" :link="'/recipes'"></HeaderLinkComponent>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
