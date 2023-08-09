b4195c8c33c4cee58009cf41b768570e09e2ce60
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-09 15:21:06 +0300

    Перенес footer в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <script setup>
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 </script>
 
 <template>
   <div>
     <v-app>
       <HeaderComponent></HeaderComponent>
-    </v-app>
-    <div>
-      <router-view></router-view>
-    </div>
 
-    <footer class="fixed-bottom bg-white">
-      <hr class="border border-dark border-2 opacity-100" />
-      <p class="text-center">miniblog@mail.ru <br /></p>
-      <p class="text-center text-body-secondary text-uppercase">
-        © copyright 2023
-      </p>
-    </footer>
+      <v-main class="mt-4 content">
+        <router-view></router-view>
+      </v-main>
+
+      <FooterComponent></FooterComponent>
+    </v-app>
   </div>
 </template>
