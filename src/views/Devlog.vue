007cd7a56bff36a5c62655caee5cf67d2a779b50
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-09 18:53:46 +0300

    Убрал форму для добавления новых записей

--- src/views/Devlog.vue
+++ src/views/Devlog.vue
@@ -14,30 +14,6 @@ function addArticel() {
 
 <template>
   <div class="center">
-    <v-sheet width="500" class="mx-auto mb-8">
-      <v-form @submit.prevent>
-        <v-text-field
-          id="devlog-title"
-          v-model="title"
-          :rules="titleRules"
-          label="Заголовок"
-        ></v-text-field>
-        <v-textarea
-          id="devlog-description"
-          v-model="description"
-          :rules="descriptionRules"
-          label="Описание"
-        ></v-textarea>
-        <v-btn
-          type="submit"
-          block
-          density="default"
-          @click="addArticel"
-          >Добавить запись</v-btn
-        >
-      </v-form>
-    </v-sheet>
-
     <div v-for="articel in devlog.articels.length" v-bind:key="articel.id">
       <ArticleComponent :id="articel - 1" />
     </div>
