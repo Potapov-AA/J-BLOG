89e289df2d112a231ca69d39ba4ece0359a684f7
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-09 15:22:20 +0300

    Добавил форму для добавления записей

--- src/views/Devlog.vue
+++ src/views/Devlog.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useDevlogStore } from "@/stores/devlog";
-import ArticleComponent from '@/components/ArticleComponent.vue'
-    
+import ArticleComponent from "@/components/ArticleComponent.vue";
+
 const devlog = useDevlogStore();
 
 function addArticel() {
@@ -13,81 +13,33 @@ function addArticel() {
 </script>
 
 <template>
-  <div class="devlog-content center">
-    <div class="text-center mb-3">
-      <button
-        type="button"
-        class="btn btn-light border border-2 text-uppercase"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-      >
-        <b>Добавить запись</b>
-      </button>
-    </div>
+  <div class="center">
+    <v-sheet width="500" class="mx-auto mb-8">
+      <v-form @submit.prevent>
+        <v-text-field
+          id="devlog-title"
+          v-model="title"
+          :rules="titleRules"
+          label="Заголовок"
+        ></v-text-field>
+        <v-textarea
+          id="devlog-description"
+          v-model="description"
+          :rules="descriptionRules"
+          label="Описание"
+        ></v-textarea>
+        <v-btn
+          type="submit"
+          block
+          density="default"
+          @click="addArticel"
+          >Добавить запись</v-btn
+        >
+      </v-form>
+    </v-sheet>
 
     <div v-for="articel in devlog.articels.length" v-bind:key="articel.id">
-      <ArticleComponent :id="articel - 1"/>
-    </div>
-
-    <!-- Модальное окно -->
-    <div
-      class="modal fade"
-      id="staticBackdrop"
-      data-bs-backdrop="static"
-      data-bs-keyboard="false"
-      tabindex="-1"
-      aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="staticBackdropLabel">
-              Добавление новой записи
-            </h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Закрыть"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="mb-3">
-                <label for="devlog-title" class="col-form-label">
-                  Название:
-                </label>
-                <input type="text" class="form-control" id="devlog-title" />
-              </div>
-              <div class="mb-3">
-                <label for="devlog-description" class="col-form-label">
-                  Описание:
-                </label>
-                <textarea class="form-control" id="devlog-description">
-                </textarea>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-light border border-2"
-              data-bs-dismiss="modal"
-              @click="addArticel"
-            >
-              Добавить
-            </button>
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Закрыть
-            </button>
-          </div>
-        </div>
-      </div>
+      <ArticleComponent :id="articel - 1" />
     </div>
   </div>
 </template>
