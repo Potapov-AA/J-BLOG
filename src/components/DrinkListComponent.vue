7ba91c93729b75e902ea7a74fd8f5373b50865ab
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-11 13:55:30 +0300

    Компонент для отображения списка напитков

---
+++ src/components/DrinkListComponent.vue
@@ -0,0 +1,52 @@
+<script setup>
+import RecipeCardComponent from "@/components/RecipeCardComponent.vue";
+
+import { useRecipeStore } from "@/stores/recipe";
+
+const recipes = useRecipeStore();
+
+function getPageCount() {
+  if (recipes.drinks.length % 2 > 0) {
+    return Math.floor(recipes.drinks.length / 2 + 1);
+  } else {
+    return Math.floor(recipes.drinks.length / 2);
+  }
+}
+
+function getCountRecipeOnPage(page) {
+  if (page == getPageCount()) {
+    if (recipes.drinks.length % 2 != 0) {
+      return recipes.drinks.length % 2;
+    }
+  }
+  return -1;
+}
+</script>
+
+<template>
+  <div>
+    <div v-for="i in getPageCount()" :key="i" cover>
+      <div
+        v-if="getCountRecipeOnPage(i) == -1"
+        class="d-flex flex-row justify-center"
+      >
+        <RecipeCardComponent
+          v-for="j in 2"
+          :key="j"
+          :id="(i - 1) * 2 + j - 1"
+          :isFood="false"
+          class="ma-5"
+        ></RecipeCardComponent>
+      </div>
+      <div v-else class="d-flex flex-row justify-center">
+        <RecipeCardComponent
+          v-for="j in getCountRecipeOnPage(i)"
+          :key="j"
+          :id="(i - 1) * 2 + j - 1"
+          :isFood="false"
+          class="ma-5"
+        ></RecipeCardComponent>
+      </div>
+    </div>
+  </div>
+</template>
