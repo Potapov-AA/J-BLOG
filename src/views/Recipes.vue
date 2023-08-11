35f6990fcdad02abd7f36f08c121e9cc287027a4
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-11 13:56:56 +0300

    Настроена страница с рецептами

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,4 +1,26 @@
+<script setup>
+import { ref} from "vue";
+
+import FoodListComponent from "@/components/FoodListComponent.vue";
+import DrinkListComponent from "@/components/DrinkListComponent.vue";
+
+
+const tabs = [FoodListComponent, DrinkListComponent];
+
+const currentList = ref(FoodListComponent);
+</script>
+
 <template>
-        <h1>Recipes</h1>
-    
+  <div class="mt-5">
+    <div class="d-flex justify-center">
+      <v-icon @click="currentList = tabs[0]" size="80" color="black mx-10">
+        mdi-noodles
+      </v-icon>
+      <v-icon @click="currentList = tabs[1]" size="80" color="black mx-10">
+        mdi-glass-mug
+      </v-icon>
+    </div>
+    <currentList></currentList>
+
+  </div>
 </template>
